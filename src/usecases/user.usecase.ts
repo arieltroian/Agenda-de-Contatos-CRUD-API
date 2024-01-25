e7c0569bede2ca7ce9e208b5c6663f4b1ad89cc3
arieltroian <arieltroian@outlook.com>
2024-01-25 12:23:15 -0300

    feat: create User UseCase

---
+++ src/usecases/user.usecase.ts
@@ -0,0 +1,12 @@
+import { User, UserCreate, UserRepository } from "../interfaces/user.interface";
+import { UserRepositoryPrisma } from "../repositories/user.repository";
+
+class UserUseCase {
+  private userRepository: UserRepository;
+  constructor() {
+    this.userRepository = new UserRepositoryPrisma();
+  }
+  async create({ name, email }: UserCreate): Promise<User> {}
+}
+
+export { UserUseCase };
