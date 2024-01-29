298190170c36c03e960f851211c35368f96400ce
arieltroian <arieltroian@outlook.com>
2024-01-29 14:56:53 -0300

    feat(routes): register contactRoutes with the prefix '/contacts'
    Adds the registration of routes defined in 'contactsRoutes' in the application, using the prefix '/contacts'.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import fastify, { FastifyInstance } from "fastify";
 import { userRoutes } from "./routes/user.routes";
+import { contactsRoutes } from "./routes/contact.routes";
 
 const app: FastifyInstance = fastify({ logger: true });
 
 app.register(userRoutes, {
   prefix: "/users",
+});
+app.register(contactsRoutes, {
+  prefix: "/contacts",
 });
 
 app.listen(
@@ -13,3 +17,5 @@ app.listen(
   },
   () => console.log("Server is running on port 3100")
 );
+
+// yarn dev
