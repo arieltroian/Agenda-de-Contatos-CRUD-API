488052f1da256ec7bb70395335978f706d9335af
arieltroian <arieltroian@outlook.com>
2024-01-29 14:53:53 -0300

    feat(contact-usecase): implement methods for creating, listing, updating and deleting contacts
    Adds the implementation of the ContactUseCase class, which uses repositories to interact with
    the database and provides methods for creating, listing, updating, and deleting contacts.

---
+++ src/usecases/contact.usecase.ts
@@ -0,0 +1,79 @@
+import {
+  Contact,
+  ContactCreate,
+  ContactRepository,
+} from "../interfaces/contacts.interface";
+import { ContactsRepositoryPrisma } from "../repositories/contacts.repository";
+import { UserRepositoryPrisma } from "../repositories/user.repository";
+
+class ContactUseCase {
+  private contactRepository: ContactRepository;
+  private userRepository: UserRepositoryPrisma;
+
+  constructor() {
+    this.contactRepository = new ContactsRepositoryPrisma();
+    this.userRepository = new UserRepositoryPrisma();
+  }
+
+  async create({ email, name, phone, userEmail }: ContactCreate) {
+    // email do usuário logado
+    // buscar o usuário pelo email
+    // se não existir, retornar erro
+    // se existir, criar contato
+    // antes de criar o contato, validar se ele já existe pelo telefone ou email
+
+    const user = await this.userRepository.findByEmail(userEmail);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const verifyIfExistsContact =
+      await this.contactRepository.findByEmailOrPhone(email, phone);
+
+    if (verifyIfExistsContact) {
+      throw new Error("Contact already exists");
+    }
+
+    const contact = await this.contactRepository.create({
+      email,
+      name,
+      phone,
+      userId: user.id,
+    });
+    return contact;
+  }
+
+  async listAllContacts(userEmail: string) {
+    const user = await this.userRepository.findByEmail(userEmail);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const contacts = await this.contactRepository.findAllContacts(user.id);
+
+    return contacts;
+  }
+
+  async updateContact({ id, name, email, phone }: Contact) {
+    const data = await this.contactRepository.updateContact({
+      id,
+      name,
+      email,
+      phone,
+    });
+
+    return data;
+  }
+
+  async delete(id: string) {
+    const data = await this.contactRepository.delete(id);
+
+    return data;
+  }
+}
+
+export { ContactUseCase };
+
+// UseCase: criação dos métodos
