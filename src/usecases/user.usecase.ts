6f05736921f2fa91f4229862808a6476a239436a
arieltroian <arieltroian@outlook.com>
2024-01-29 14:41:53 -0300

    feat(user-use-case): add user creation method with existence check
    Adds 'create' method to UserUseCase class, which allows creating a new user
    after checking if the user with the same email already exists. If it exists, throw an exception.

--- src/usecases/user.usecase.ts
+++ src/usecases/user.usecase.ts
@@ -6,7 +6,17 @@ class UserUseCase {
   constructor() {
     this.userRepository = new UserRepositoryPrisma();
   }
-  async create({ name, email }: UserCreate): Promise<User> {}
+  async create({ name, email }: UserCreate): Promise<User> {
+    const verifyIfUserExists = await this.userRepository.findByEmail(email);
+    if (verifyIfUserExists) {
+      throw new Error("User already exists");
+    }
+    const result = await this.userRepository.create({ email, name });
+
+    return result;
+  }
 }
 
 export { UserUseCase };
+
+// UseCase: criação dos métodos
